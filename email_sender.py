d278ecf5ed2f00b34ce39989a806486f52269b25
loogle18 <sme@jetthoughts.com>
2018-01-25 02:16:41 +0200

    Added try except for sending emails.

--- email_sender.py
+++ email_sender.py
@@ -5,16 +5,19 @@ from config import email_sender_address, email_sender_password
 
 
 def send_email_message(mailto, message):
-    msg = MIMEMultipart()
-    msg["From"] = email_sender_address
-    msg["To"] = mailto
-    msg["Subject"] = "Uber Prices Estimator"
-    msg.attach(MIMEText(message, "plain"))
+    try:
+        msg = MIMEMultipart()
+        msg["From"] = email_sender_address
+        msg["To"] = mailto
+        msg["Subject"] = "Uber Prices Estimator"
+        msg.attach(MIMEText(message, "plain"))
 
-    server = SMTP("smtp.gmail.com:587")
-    server.starttls()
-    server.login(email_sender_address, email_sender_password)
-    text = msg.as_string()
-    server.sendmail(email_sender_address, mailto, text)
-    server.quit()
-    print("Sent email to %s. Message: \n %s" % (mailto, message))
+        server = SMTP("smtp.gmail.com:587")
+        server.starttls()
+        server.login(email_sender_address, email_sender_password)
+        text = msg.as_string()
+        server.sendmail(email_sender_address, mailto, text)
+        server.quit()
+        print("Sent email to %s. Message: \n %s" % (mailto, message))
+    except Exception as e:
+        print(e)
